6f71517b14b1f569ee7df7e084d227cf360d774e
tsk@antares.no <tsk@antares.no@7cf2ceec-e8bc-6c4d-9072-75b59bd30057>
2012-03-26 19:55:27 +0000

    Pulled database specifics out of DBWrapper

--- trunk/src/main/scala/no/antares/dbunit/DbWrapper.scala
+++ trunk/src/main/scala/no/antares/dbunit/DbWrapper.scala
@@ -21,67 +21,19 @@ import org.dbunit.operation.DatabaseOperation
 import org.xml.sax.InputSource
 import org.dbunit.dataset.xml.{FlatXmlDataSet, FlatXmlDataSetBuilder}
 import xml.{XML, Node}
-import org.dbunit.database.{ForwardOnlyResultSetTableFactory, DatabaseConfig, QueryDataSet, DatabaseConnection}
+import org.dbunit.database.{ForwardOnlyResultSetTableFactory, DatabaseConfig, QueryDataSet}
 import org.dbunit.util.fileloader.FlatXmlDataFileLoader
 import java.io._
-import java.util.Properties
-import java.sql.{DriverManager, Connection}
 import org.apache.commons.io.IOUtils
-import org.apache.tools.ant.taskdefs.SQLExec
-import org.apache.tools.ant.Project
-import io.{BufferedSource, Source}
-import no.antares.util.FileUtil
-import converters.DefaultNameConverter
-import collection.mutable.ListBuffer
-import org.apache.derby.tools.ij
-import javax.management.remote.rmi._RMIConnection_Stub
-import org.dbunit.dataset.csv.CsvDataSet
 import org.dbunit.dataset.stream.DataSetProducerAdapter
 import org.codehaus.jettison.json.JSONObject
 
 /** Common Code for database
  * @author Tommy Skodje
 */
-class DbWrapper( val properties: DbProperties ) {
+class DbWrapper( val db: Db ) {
 
   private final val logger: Logger = LoggerFactory.getLogger( classOf[DbWrapper] )
-
-  private val dbUnitProperties  = new ListBuffer[ Tuple2[String, Object] ]();
-
-  if ( properties.driver.endsWith( "OracleDriver" ) ) {
-    dbUnitProperties.append( ( DatabaseConfig.FEATURE_SKIP_ORACLE_RECYCLEBIN_TABLES, true.asInstanceOf[Object] ) );
-    dbUnitProperties.append( ( DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new org.dbunit.ext.oracle.OracleDataTypeFactory() ) );
-  } else if ( properties.driver.startsWith( "org.apache.derby" ) )
-    dbUnitProperties.append( ( DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, false.asInstanceOf[Object] ) )
-
-  loadDriver( properties.driver );
-  protected val dbConnection: Connection	= connect()
-
-  protected def connection(): Connection	= dbConnection;
-
-  def getDbUnitConnection(): DatabaseConnection = {
-    val dbuConnection =
-      if ( properties.schema.isEmpty )
-        new DatabaseConnection( connection );
-      else
-        new DatabaseConnection( connection, properties.schema );
-    val config = dbuConnection.getConfig();
-    dbUnitProperties.foreach( property => config.setProperty( property._1, property._2 ) );
-    dbuConnection
-  }
-
-  protected def connect(): Connection	= {
-    val connectionUrl = properties.dbUrl;
-    val connectionProperties = new Properties();
-    connectionProperties.put( "user", properties.schema );
-    connectionProperties.put( "username", properties.username );
-    connectionProperties.put( "password", properties.password );
-
-    DriverManager.getConnection( connectionUrl, connectionProperties )
-  }
-
-  /**  */
-  def rollback(): Unit	= connection.rollback()
 
   /**  */
   def refreshWithFlatJSON( json: JsonDataSet ): Unit = {
@@ -178,42 +130,26 @@ class DbWrapper( val properties: DbProperties ) {
   /**  */
   def runSqlScript( script: Encoded[ InputStream ] ): Boolean = {
     try {
-      connection.setAutoCommit(false);
-      val result = runSqlScript( script, new Encoded[ OutputStream ]( System.out ) );
-      connection.commit();
-      return result;
+      return doInTransaction{ () => runSqlScript( script, new Encoded[ OutputStream ]( System.out ) ); }
     } finally {
       catchall { () => script.stream.close() };
     }
   }
 
+
   def runSqlScript( script: Encoded[ InputStream ] , output: Encoded[ OutputStream ] ): Boolean = {
     val writer = new StringWriter();
     IOUtils.copy( script.stream, writer, script.encoding )
-    executeSql( writer.toString() )
+    db.executeSql( writer.toString() )
   }
 
-  /** Use ant (http://stackoverflow.com/questions/2071682/how-to-execute-sql-script-file-in-java) */
-  def executeSql( script: String ): Boolean = {
-    try {
-      val executer = new SQLExec()
-      val project = new Project();
-      project.init();
-      executer.setProject(project);
-      executer.setTaskType("sql");
-      executer.setTaskName("sql");
+  private def getDbUnitConnection() = db.getDbUnitConnection();
 
-      // executer.setSrc(new File(sqlFilePath));
-      executer.setDriver( properties.driver );
-      executer.setPassword( properties.password );
-      executer.setUserid( properties.username );
-      executer.setUrl( properties.dbUrl );
-      executer.addText( script )
-      executer.execute();
-    } catch {
-      case t: Throwable => return false;
-    }
-    true;
+  def doInTransaction[T]( f : () => T ): T = {
+    db.connection().setAutoCommit(false);
+    val result = f();
+    db.connection().commit();
+    result
   }
 
   /** Bundles stream with encoding, default to UTF-8 */
@@ -229,41 +165,6 @@ class DbWrapper( val properties: DbProperties ) {
        f()
     } catch {
       case t: Throwable => ;	// ignore
-    }
-  }
-
-  /**
-   * Loads the appropriate JDBC driver for this environment/framework. For
-   * example, if we are in an embedded environment, we load Derby's
-   * embedded Driver, <code>org.apache.derby.jdbc.EmbeddedDriver</code>.
-   */
-  protected def loadDriver( driver: String ): Unit = {
-    /*
-     *  The JDBC driver is loaded by loading its class.
-     *  If you are using JDBC 4.0 (Java SE 6) or newer, JDBC drivers may
-     *  be automatically loaded, making this code optional.
-     *
-     *  In an embedded environment, this will also start up the Derby
-     *  engine (though not any databases), since it is not already
-     *  running. In a client environment, the Derby engine is being run
-     *  by the network server framework.
-     *
-     *  In an embedded environment, any static Derby system properties
-     *  must be set before loading the driver to take effect.
-     */
-    try {
-      Class.forName(driver).newInstance();
-      logger.debug( "Loaded the appropriate driver" );
-    } catch {
-      case ex: ClassNotFoundException => {
-        logger.error( "\nUnable to load the JDBC driver " + driver + "\nPlease check your CLASSPATH." , ex );
-      }
-      case ex: InstantiationException => {
-        logger.error( "\nUnable to instantiate the JDBC driver " + driver, ex );
-      }
-      case ex: IllegalAccessException => {
-        logger.error( "\nNot allowed to access the JDBC driver " + driver, ex );
-      }
     }
   }
 
